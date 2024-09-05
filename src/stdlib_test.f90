95eca0eb6729e6ea24f4fcfac1b312919648b55d
khaors <khaors@gmail.com>
2024-09-05 18:55:11 -0500

    test files added

--- src/stdlib_test.f90
+++ src/stdlib_test.f90
@@ -1,10 +1,25 @@
 module stdlib_test
+  use stdlib_kinds, only: dp,int8,int32
   implicit none
   private
-
-  public :: say_hello
+!
+  public :: test_stdlib_kind
+!
 contains
-  subroutine say_hello
-    print *, "Hello, stdlib_test!"
-  end subroutine say_hello
+!
+  subroutine test_stdlib_kind
+!
+    real(kind=dp) :: value 
+    integer(int8) :: value1
+    integer(int32) :: value2
+    write(*,*) "Hello, stdlib_test!"
+    value=1.24335788_dp;
+    write(*,*) 'value= ',value;
+    value1=9_int8
+    write(*,*) 'value1= ',value1;
+    value2=19356777_int32;
+    write(*,*) 'value2= ',value2;
+!
+  end subroutine test_stdlib_kind
+!
 end module stdlib_test
