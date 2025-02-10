0c2406345fba178fef46635427e2a79adafb1f01
khaors <khaors@gmail.com>
2025-02-09 22:05:17 -0500

    File main.f90 removed

--- app/main.f90
+++
@@ -1,9 +0,0 @@
-program main
-  use stdlib_test, only: test_stdlib_kind
-  use stdlib_logger_test,only: test_logger;
-  implicit none
-!
-  call test_stdlib_kind();
-  call test_logger();
-!  
-end program main
